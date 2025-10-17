9e581a796ba79232971ef3a8e134e393a1bf13d9
ashrafmusa <ashraf096849109@gmail.com>
2025-10-18 03:29:47 +0400

    .

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base: '/accreditex/',
     plugins: [react()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
